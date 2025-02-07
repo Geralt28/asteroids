b28c10cf5fa434addd6fab9c7c4ac75e619995bc
Geralt <przemyslaw26@interia.pl>
2025-02-07 23:47:51 +0100

    Dodany statek gracza

--- main.py
+++ main.py
@@ -4,25 +4,36 @@
 import pygame
 
 from constants import *
+#from circleshape import *
+from player import *
 
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    player.containers = (updatable, drawable)
+    updatable.add(player)
+    drawable.add(player)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, color="black")
+        screen.fill(color="black")
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
      
-
-
-
 
 if __name__ == "__main__":
     main()
